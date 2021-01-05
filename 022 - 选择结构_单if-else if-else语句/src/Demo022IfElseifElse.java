78257b522e57d8f4581f91a4ecd268e75fee346d
foxthere <weidufox@gmail.com>
2021-01-05 16:37:32 +0300

    增加 022 - 选择结构_单if-else if-else语句

---
+++ 022 - 选择结构_单if-else if-else语句/src/Demo022IfElseifElse.java
@@ -0,0 +1,15 @@
+// 022 - 选择结构_单if-else if-else语句
+
+public class Demo022IfElseifElse {
+    public static void main(String[] args) {
+        int num = -10;
+
+        if (num > 0) {
+            System.out.println("大于0");
+        } else if (num < 0) {
+            System.out.println("小于0");
+        } else {
+            System.out.println("等于0");
+        }
+    }
+}
