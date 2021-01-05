4429239c4ef419a9e894d1b0e6670e91a97de3a1
foxthere <weidufox@gmail.com>
2021-01-05 16:13:28 +0300

    增加 021 - 选择结构_单if-else语句

---
+++ 021 - 选择结构_单if-else语句/src/Demo021IfElse.java
@@ -0,0 +1,11 @@
+public class Demo021IfElse {
+    public static void main(String[] args) {
+        int num = 13;
+
+        if (num % 2 == 0) {
+            System.out.println("偶数");
+        } else {
+            System.out.println("奇数");
+        }
+    }
+}
