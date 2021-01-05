4a4d662140e1b8269c860a5998fe39d5bde5eb52
foxthere <weidufox@gmail.com>
2021-01-05 18:16:53 +0300

    增加 030 - 循环结构_for循环

---
+++ 030 - 循环结构_for循环/src/Demo030LoopFor.java
@@ -0,0 +1,9 @@
+// 030 - 循环结构_for循环
+
+public class Demo030LoopFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; ++i) {
+            System.out.println(i);
+        }
+    }
+}
