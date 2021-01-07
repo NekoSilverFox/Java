873d01516e3c091d5fb586844b45db57db279990
foxthere <weidufox@gmail.com>
2021-01-07 17:26:11 +0300

    增加 方法调用

--- 100 - OOP_类的定义/src/Demo100Student.java
+++ 100 - OOP_类的定义/src/Demo100Student.java
@@ -33,5 +33,21 @@ public class Demo100Student {
         // 对象名.成员变量名
         System.out.println(stu.name); // null
         System.out.println(stu.age); // 0
+
+        System.out.println("=============");
+
+        // 改变对象当中的成员变量数值内容
+        // 将右侧的字符串，赋值交给stu对象当中的name成员变量
+        stu.name = "赵丽颖";
+        stu.age = 18;
+        System.out.println(stu.name); // 赵丽颖
+        System.out.println(stu.age); // 18
+        System.out.println("=============");
+
+        // 4. 使用对象的成员方法，格式：
+        // 对象名.成员方法名()
+        stu.eat();
+        stu.sleep();
+        stu.study();
     }
 }
