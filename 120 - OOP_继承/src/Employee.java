817d0121c9bd73a949b2e4151006c3c0911041b1
foxthere <weidufox@gmail.com>
2021-01-20 20:56:02 +0300

    增加 120 - OOP_继承

---
+++ 120 - OOP_继承/src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    public Employee() {
+        System.out.println("Employee 的构造函数执行了");
+    }
+
+    public void method() {
+        System.out.println("Employee 中的method函数执行了！");
+    }
+}
---
+++ 120 - OOP_继承/src/Teacher.java
@@ -0,0 +1,5 @@
+public class Teacher extends Employee {
+    public Teacher() {
+        System.out.println("Teacher 的构造函数执行了！");
+    }
+}
