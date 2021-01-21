8a532e7f7b0688d6c67ee2aff33d4e6103d9a781
foxthere <weidufox@gmail.com>
2021-01-21 18:36:04 +0300

    继承关系中，父子类构造方法的访问特点：
        1. 子类构造方法当中有一个默认隐含的“super()”调用，所以一定是先调用的父类构造，后执行的子类构造。
        2. 子类构造可以通过super关键字来调用父类重载构造。
        3. super的父类构造调用，必须是子类构造方法的第一个语句。不能一个子类构造调用多次super构造。
        总结：
        子类必须调用父类构造方法，不写则赠送super()；写了则用写的指定的super调用，super只能有一个，还必须是第一个。

--- 120 - OOP_继承/src/Employee.java
+++ 120 - OOP_继承/src/Employee.java
@@ -2,10 +2,13 @@ public class Employee {
     int num = 1;
 
     public Employee() {
+        super(); // 在调用父类无参构造方法
+//        super(20); // 在调用父类重载的构造方法
         System.out.println("Employee 的构造函数执行了");
     }
 
     public void method() {
+        //        super(); // 错误写法！只有子类构造方法，才能调用父类构造方法。
         System.out.println("Employee 中的method函数执行了！");
     }
 }
