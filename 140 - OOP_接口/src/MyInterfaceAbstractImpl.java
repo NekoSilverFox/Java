557ebe4be2a92b9213efaf86f8d6084e9999b968
foxthere <weidufox@gmail.com>
2021-01-23 00:05:49 +0300

    增加 抽象方法的基本使用

---
+++ 140 - OOP_接口/src/MyInterfaceAbstractImpl.java
@@ -0,0 +1,17 @@
+public class MyInterfaceAbstractImpl implements MyInterfaceAbstract {
+    @Override
+    public void methodAbs() {
+        System.out.println("NO.1 method");
+    }
+
+    @Override
+    public void methodAbs2() {
+        System.out.println("NO.2 method");
+
+    }
+
+    @Override
+    public void methodAbs3() {
+        System.out.println("NO.3 method");
+    }
+}
