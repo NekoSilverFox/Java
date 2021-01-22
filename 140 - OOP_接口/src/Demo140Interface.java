50b4319a5fb791f10034f99dd54fe616ad553238
foxthere <weidufox@gmail.com>
2021-01-23 00:32:15 +0300

    增加 接口中的静态使用方法         注意事项：不能通过接口实现类的对象来调用接口当中的静态方法。         正确用法：通过接口名称，直接调用其中的静态方法。         格式：         接口名称.静态方法名(参数);

--- 140 - OOP_接口/src/Demo140Interface.java
+++ 140 - OOP_接口/src/Demo140Interface.java
@@ -61,5 +61,21 @@ public interface Demo140Interface {
         MyInterfaceDefaultB b = new MyInterfaceDefaultB();
         b.methodAbs();
         b.methodDefault();
+
+
+        /*
+        注意事项：不能通过接口实现类的对象来调用接口当中的静态方法。
+        正确用法：通过接口名称，直接调用其中的静态方法。
+        格式：
+        接口名称.静态方法名(参数);
+         */
+        System.out.println("===========");
+        // 错误写法！
+//        MyInterfaceStaticImpl s = new MyInterfaceStaticImpl();
+//        s.help();
+
+        // 直接通过接口名称调用静态方法
+        MyInterfaceStatic.help();
+
     }
 }
---
+++ 140 - OOP_接口/src/MyInterfaceStaticImpl.java
@@ -0,0 +1,3 @@
+public class MyInterfaceStaticImpl implements MyInterfaceStatic {
+
+}
