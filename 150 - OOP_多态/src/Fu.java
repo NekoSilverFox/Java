554e27654341d7b512d317d72b58643b9e6c3f0b
foxthere <weidufox@gmail.com>
2021-01-23 17:56:55 +0300

    增加 访问成员变量和方法的方式【重点】    在多态的代码当中，成员【方法】的访问规则是：        看new的是谁，就优先用谁，没有则向上找。    口诀：编译看左边，运行看右边。
       访问成员变量的两种方式：
       成员变量不能覆盖重写【重点】
       1. 直接通过对象名称访问成员变量：看等号左边是谁，优先用谁，没有则向上找。
       2. 间接通过成员方法访问成员变量：看该方法属于谁，优先用谁，没有则向上找。
    
       对比一下：【重点】
       成员变量：编译看左边，运行还看左边。
       成员方法：编译看左边，运行看右边。

--- 150 - OOP_多态/src/Fu.java
+++ 150 - OOP_多态/src/Fu.java
@@ -1,9 +1,15 @@
 public class Fu {
-    public void method () {
+    int num = 10;
+
+    public void method() {
         System.out.println("Fu");
     }
 
     public void methodFu() {
         System.out.println("Only Fu");
+    }
+
+    public void showNum() {
+        System.out.println(num);
     }
 }
--- 150 - OOP_多态/src/Zi.java
+++ 150 - OOP_多态/src/Zi.java
@@ -1,6 +1,17 @@
 public class Zi extends Fu{
+    int num = 20;
+
     @Override
     public void method () {
         System.out.println("Zi");
+    }
+
+    @Override
+    public void showNum() {
+        System.out.println(num);
+    }
+
+    public void MethodZi() {
+
     }
 }
