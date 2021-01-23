ef0c1715c8d8a86243a3b5007dcfbf747878ec17
foxthere <weidufox@gmail.com>
2021-01-23 17:25:18 +0300

    修改 150 - OOP_多态    代码当中体现多态性，其实就是一句话：父类引用指向子类对象。
       格式：
       父类名称 对象名 = new 子类名称();
       或者：
       接口名称 对象名 = new 实现类名称();

---
+++ 150 - OOP_多态/src/Fu.java
@@ -0,0 +1,9 @@
+public class Fu {
+    public void method () {
+        System.out.println("Fu");
+    }
+
+    public void methodFu() {
+        System.out.println("Only Fu");
+    }
+}
---
+++ 150 - OOP_多态/src/Zi.java
@@ -0,0 +1,6 @@
+public class Zi extends Fu{
+    @Override
+    public void method () {
+        System.out.println("Zi");
+    }
+}
