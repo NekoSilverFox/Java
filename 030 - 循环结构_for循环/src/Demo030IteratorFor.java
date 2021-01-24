b6a60880504f5ee897933aa44702ff3ca3e738cc
foxthere <weidufox@gmail.com>
2021-01-24 14:55:33 +0300

    增加 Demo030IteratorFor

---
+++ 030 - 循环结构_for循环/src/Demo030IteratorFor.java
@@ -0,0 +1,8 @@
+public class Demo030IteratorFor {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8};
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
