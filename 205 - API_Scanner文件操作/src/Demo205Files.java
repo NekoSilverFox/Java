78959335f0861aa8846493090b06929e0ccbe6b8
foxthere <weidufox@gmail.com>
2021-02-20 17:57:36 +0300

    增加 【失败】205 - API_Scanner文件操作

---
+++ 205 - API_Scanner文件操作/src/Demo205Files.java
@@ -0,0 +1,16 @@
+// 205 - API_Scanner文件操作
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+public class Demo205Files {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        // 写入文件，构造一个PrintWriter对象
+        PrintWriter fileOut = new PrintWriter("myFile.txt", String.valueOf(StandardCharsets.UTF_8));
+
+        fileOut.println("123");
+        fileOut.println(321);;
+    }
+}
