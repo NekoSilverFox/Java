eb3c5d6ba4d48e382718fdce95fac665f8857f9c
foxthere <weidufox@gmail.com>
2021-03-25 16:17:42 +0300

    增加 355 - 多线程_使用this的同步代码块

---
+++ 355 - 多线程_使用this的同步代码块/src/DemoSynchronized.java
@@ -0,0 +1,39 @@
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2021/3/25 14:35
+ * @Author : NekoSilverfox
+ * @FileName: DemoSynchronized
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ */
+/*
+    卖票案例出现了线程安全问题
+    卖出了不存在的票和重复的票
+
+    解决线程安全问题的一种方案:使用同步代码块
+    格式:
+        synchronized(锁对象){
+            可能会出现线程安全问题的代码(访问了共享数据的代码)
+        }
+
+    注意:
+        1.通过代码块中的锁对象,可以使用任意的对象
+        2.但是必须保证多个线程使用的锁对象是同一个
+        3.锁对象作用:
+            把同步代码块锁住,只让一个线程在同步代码块中执行
+ */
+public class DemoSynchronized {
+    public static void main(String[] args) {
+        RunnableImpl runnable = new RunnableImpl();
+        System.out.println("main-> " + runnable);
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+        Thread t3 = new Thread(runnable);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
---
+++ 356 - 多线程_同步方法/src/DemoSynchronizedMethod.java
@@ -0,0 +1,15 @@
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2021/3/25 15:13
+ * @Author : NekoSilverfox
+ * @FileName: DemoSynchronizedMethod
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ *
+ * 355 - 多线程_同步方法
+ */
+
+public class DemoSynchronizedMethod {
+}
