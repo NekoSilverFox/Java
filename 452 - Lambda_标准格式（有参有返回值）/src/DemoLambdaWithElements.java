96249c0aaaa2d7cb3e7e8e5c59442ef251250c5b
foxthere <weidufox@gmail.com>
2021-03-26 22:09:23 +0300

    增加 452 - Lambda_标准格式（有参有返回值） Lambda表达式的标准格式:     由三部分组成:         a.一些参数         b.一个箭头         c.一段代码     格式:         (参数列表) -> {一些重写方法的代码};     解释说明格式:         ():接口中抽象方法的参数列表,没有参数,就空着;有参数就写出参数,多个参数使用逗号分隔         ->:传递的意思,把参数传递给方法体{}         {}:重写接口的抽象方法的方法体  Arrays.sort(arrPerson, (l, r)->{return l.getAge() - r.getAge();});

---
+++ 452 - Lambda_标准格式（有参有返回值）/src/DemoLambdaWithElements.java
@@ -0,0 +1,50 @@
+import java.lang.ref.PhantomReference;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2021/3/26 21:56
+ * @Author : NekoSilverfox
+ * @FileName: DemoLambdaWithElements
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ *
+ * 452 - Lambda_标准格式（有参有返回值）
+ */
+
+public class DemoLambdaWithElements {
+    public static void main(String[] args) {
+        Person[] arrPerson = {
+                new Person("Fox", 17),
+                new Person("Silver", 15),
+                new Person("Red Panda", 14),
+                new Person("Tom", 18)
+        };
+
+        // 对数组中的Person对象使用Arrays的sort方法通过年龄进行排序（前面-后面）
+//        Arrays.sort(arrPerson, new Comparator<Person>() {
+//            @Override
+//            public int compare(Person o1, Person o2) {
+//                return o1.getAge() - o2.getAge();
+//            }
+//        });
+
+        // 使用Lambda表达式
+        Arrays.sort(arrPerson, (Person l, Person r)->{
+            return l.getAge() - r.getAge();
+        });
+
+        // 优化省略Lambda
+        Arrays.sort(arrPerson, (l, r)->{return l.getAge() - r.getAge();});
+
+
+        for (Person p : arrPerson) {
+            System.out.println(p);
+        }
+
+    }
+}
---
+++ 452 - Lambda_标准格式（有参有返回值）/src/Person.java
@@ -0,0 +1,47 @@
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2021/3/26 21:54
+ * @Author : NekoSilverfox
+ * @FileName: Person
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ */
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
