0599a9e12f7617de85aa9aad84b8859612ad25de
foxthere <weidufox@gmail.com>
2021-04-02 15:53:42 +0300

    IoC (Inversion of Control) 控制反转，是一种设计思想，DI（依赖注入）是实现IoC的一种方法 将服务的创建交给了用户，主动权在用户，用户可以根据需求来调用相应的业务层。使得程序的灵活性更高了 控制反转IoC(Inversion of Control)是说创建对象的控制权进行转移，以前创建对象的主动权和创建时机是由自己把控的， 而现在这种权力转移到第三方

--- 820 - IOC_理论推导/src/test/MyTest.java
+++ 820 - IOC_理论推导/src/test/MyTest.java
@@ -18,6 +18,10 @@ import main.com.foxthere.service.UserServiceImpl;
 
 /**
  * IoC (Inversion of Control) 控制反转，是一种设计思想，DI（依赖注入）是实现IoC的一种方法
+ * 将服务的创建交给了用户，主动权在用户，用户可以根据需求来调用相应的业务层。使得程序的灵活性更高了
+ *
+ * 控制反转IoC(Inversion of Control)是说创建对象的控制权进行转移，以前创建对象的主动权和创建时机是由自己把控的，
+ * 而现在这种权力转移到第三方
  */
 
 public class MyTest {
