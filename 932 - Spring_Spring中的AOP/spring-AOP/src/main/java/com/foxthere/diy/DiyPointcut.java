670ac41b659e6c47cf2ab2400a502edfcd50052d
foxthere <weidufox@gmail.com>
2021-04-05 23:49:46 +0300

    增加 932 - Spring中的AOP（方法2通过自定义注入）

--- 932 - Spring_Spring中的AOP/spring-AOP/src/main/java/com/foxthere/diy/DiyPointcut.java
+++ 932 - Spring_Spring中的AOP/spring-AOP/src/main/java/com/foxthere/diy/DiyPointcut.java
@@ -1,21 +1,21 @@
 /**
  * -*- coding: utf-8 -*-
  *
- * @Time : 2021/4/5 23:14
+ * @Time : 2021/4/5 23:42
  * @Author : NekoSilverfox
- * @FileName: DiyPointcut
+ * @FileName: diyPointCut
  * @Software: IntelliJ IDEA
  * @Versions: v0.1
  * @Github ：https://github.com/NekoSilverFox
  */
 package com.foxthere.diy;
 
-public class DiyPointcut {
+public class diyPointCut {
     public void before() {
-        System.out.println("============方法执行前============");
+        System.out.println("================== 方法执行前 ==================");
     }
 
     public void after() {
-        System.out.println("============方法执行后============");
+        System.out.println("================== 方法执行后 ==================");
     }
 }
