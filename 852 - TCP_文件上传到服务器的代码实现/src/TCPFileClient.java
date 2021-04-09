dfe03b34f0a7f4c3b70d21e688349bec1b863f2b
foxthere <weidufox@gmail.com>
2021-04-09 16:27:13 +0300

    增加 852 - TCP_文件上传到服务器     文件上传案例的客户端:读取本地文件,上传到服务器,读取服务器回写的数据     明确:         数据源:c:\\1.jpg         目的地:服务器     实现步骤:         1.创建一个本地字节输入流FileInputStream对象,构造方法中绑定要读取的数据源         2.创建一个客户端Socket对象,构造方法中绑定服务器的IP地址和端口号         3.使用Socket中的方法getOutputStream,获取网络字节输出流OutputStream对象         4.使用本地字节输入流FileInputStream对象中的方法read,读取本地文件         5.使用网络字节输出流OutputStream对象中的方法write,把读取到的文件上传到服务器         6.使用Socket中的方法getInputStream,获取网络字节输入流InputStream对象         7.使用网络字节输入流InputStream对象中的方法read读取服务回写的数据         8.释放资源(FileInputStream,Socket) ===========================================     文件上传案例服务器端:读取客户端上传的文件,保存到服务器的硬盘,给客户端回写"上传成功"
        明确:
            数据源:客户端上传的文件
            目的地:服务器的硬盘 d:\\upload\\1.jpg
    
        实现步骤:
            1.创建一个服务器ServerSocket对象,和系统要指定的端口号
            2.使用ServerSocket对象中的方法accept,获取到请求的客户端Socket对象
            3.使用Socket对象中的方法getInputStream,获取到网络字节输入流InputStream对象
            4.判断d:\\upload文件夹是否存在,不存在则创建
            5.创建一个本地字节输出流FileOutputStream对象,构造方法中绑定要输出的目的地
            6.使用网络字节输入流InputStream对象中的方法read,读取客户端上传的文件
            7.使用本地字节输出流FileOutputStream对象中的方法write,把读取到的文件保存到服务器的硬盘上
            8.使用Socket对象中的方法getOutputStream,获取到网络字节输出流OutputStream对象
            9.使用网络字节输出流OutputStream对象中的方法write,给客户端回写"上传成功"
            10.释放资源(FileOutputStream,Socket,ServerSocket)

---
+++ 852 - TCP_文件上传到服务器的代码实现/src/TCPFileClient.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2021/4/9 15:38
+ * @Author : NekoSilverfox
+ * @FileName: TCPFileClient
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ */
+// 852 - TCP_文件上传到服务器
+/*
+    文件上传案例的客户端:读取本地文件,上传到服务器,读取服务器回写的数据
+
+    明确:
+        数据源:c:\\1.jpg
+        目的地:服务器
+
+    实现步骤:
+        1.创建一个本地字节输入流FileInputStream对象,构造方法中绑定要读取的数据源
+        2.创建一个客户端Socket对象,构造方法中绑定服务器的IP地址和端口号
+        3.使用Socket中的方法getOutputStream,获取网络字节输出流OutputStream对象
+        4.使用本地字节输入流FileInputStream对象中的方法read,读取本地文件
+        5.使用网络字节输出流OutputStream对象中的方法write,把读取到的文件上传到服务器
+        6.使用Socket中的方法getInputStream,获取网络字节输入流InputStream对象
+        7.使用网络字节输入流InputStream对象中的方法read读取服务回写的数据
+        8.释放资源(FileInputStream,Socket)
+ */
+public class TCPFileClient {
+    public static void main(String[] args) throws IOException {
+
+        // 1.创建一个本地字节输入流FileInputStream对象,构造方法中绑定要读取的数据源
+        FileInputStream fileInputStream = new FileInputStream("852 - TCP_文件上传到服务器的代码实现\\LocalDisk\\silverfox.gif");
+
+        // 2.创建一个客户端Socket对象,构造方法中绑定服务器的IP地址和端口号
+        Socket socket = new Socket("127.0.0.1", 8080);
+
+        // 3.使用Socket中的方法getOutputStream,获取网络字节输出流OutputStream对象
+        OutputStream socketOutputStream = socket.getOutputStream();
+
+        // 4.使用本地字节输入流FileInputStream对象中的方法read,读取本地文件
+        byte[] bytes = new byte[1024];
+        int len = 0;
+        while ((len = fileInputStream.read(bytes)) != -1) {
+            // 5.使用网络字节输出流OutputStream对象中的方法write,把读取到的文件上传到服务器
+            socketOutputStream.write(bytes, 0, len);
+        }
+
+        /* 【重点】如果不发送结束标记，服务器端会进入到阻塞状态（一直while死循环）
+            解决:上传完文件,给服务器写一个结束标记
+            void shutdownOutput() 禁用此套接字的输出流。
+            对于 TCP 套接字，任何以前写入的数据都将被发送，并且后跟 TCP 的正常连接终止序列。
+         */
+        socket.shutdownOutput();
+
+        // 6.使用Socket中的方法getInputStream,获取网络字节输入流InputStream对象
+        InputStream socketInputStream = socket.getInputStream();
+
+        // 7.使用网络字节输入流InputStream对象中的方法read读取服务回写的数据
+        while ((len = socketInputStream.read(bytes)) != -1) {
+            System.out.println(new String(bytes, 0, len));
+        }
+
+        // 8.释放资源(FileInputStream,Socket)
+        fileInputStream.close();
+        socket.close();
+    }
+}
