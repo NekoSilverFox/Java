d1a58b3ab78aacf0a867a2c02b71e79927058926
foxthere <weidufox@gmail.com>
2021-04-11 01:17:47 +0300

    增加 485 - 方法引用_通过this引用本类的成员方法

---
+++ 485 - 方法引用_通过this引用本类的成员方法/src/Husband.java
@@ -0,0 +1,44 @@
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2021/4/11 1:09
+ * @Author : NekoSilverfox
+ * @FileName: Husband
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ */
+// 485 - 方法引用_通过this引用本类的成员方法
+public class Husband {
+    // 定义一个买房子的方法
+    public void buyHouse() {
+        System.out.println("北京二环内买一套四合院!");
+    }
+
+    // 定义一个结婚的方法,参数传递Richable接口
+    public void marry(Richable richable) {
+        richable.buy();
+    }
+
+    // 定义一个非常高兴的方法
+    public void soHappy() {
+        //调用结婚的方法,方法的参数Richable是一个函数式接口,传递Lambda表达式
+        marry(() -> {
+            // 使用this.成员方法,调用本类买房子的方法
+            this.buyHouse();
+        });
+
+        /*
+            使用方法引用优化Lambda表达式
+            this是已经存在的
+            本类的成员方法buyHouse也是已经存在的
+            所以我们可以直接使用this引用本类的成员方法buyHouse
+         */
+        marry(this::buyHouse);
+
+    }
+
+    public static void main(String[] args) {
+        new Husband().soHappy();
+    }
+}
---
+++ 485 - 方法引用_通过this引用本类的成员方法/src/Richable.java
@@ -0,0 +1,18 @@
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2021/4/11 1:09
+ * @Author : NekoSilverfox
+ * @FileName: Richable
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ */
+/*
+    定义一个富有的函数式接口
+ */
+@FunctionalInterface
+public interface Richable {
+    // 定义一个想买什么就买什么的方法
+    void buy();
+}
