7b1dc3c5866236b6ce50a10149c8b43136f2c7c6
foxthere <weidufox@gmail.com>
2021-04-22 14:32:32 +0300

    完善 945 - SpringMVC_使用注解开发 的注释 @Controller //@RequestMapping("/hello")  // 相当于所有前一个路径 // localhost:8080/hello/...... public class HelloController {
        /** 这个 controller 的执行过程：
         *      浏览器访问 localhost:8080/h1
         *      这个映射器返回了（这个函数） `hello`
         *      这个 hello 被视图解析器拼接后就是：/WEB-INF/jsp/hello.jsp
         *      然后 /WEB-INF/jsp/hello.jsp 中的资源就会被访问到
         *
         * @param model
         * @return
         */
        @RequestMapping("/h1")  // 浏览器访问--> localhost:8080/h1
        public String hello(Model model) {
            // 封装数据
            // 向模型中添加属性 msg 与值，可以在JSP页面中取出并渲染
            model.addAttribute("msg", "Hello Silverfox here is SpringMVC use annotation printing!!!");
    
            return "hello";  // 会被视图解析器处理，解析 --> /WEB-INF/jsp/hello.jsp 中的内容
        }
    }

--- 945 - SpringMVC_使用注解开发/sptingmvc-useAnnotation/src/main/java/com/foxthere/controller/HelloController.java
+++ 945 - SpringMVC_使用注解开发/sptingmvc-useAnnotation/src/main/java/com/foxthere/controller/HelloController.java
@@ -18,12 +18,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //@RequestMapping("/hello")  // 相当于所有前一个路径 // localhost:8080/hello/......
 public class HelloController {
 
-    @RequestMapping("/h1")
+    /** 这个 controller 的执行过程：
+     *      浏览器访问 localhost:8080/h1
+     *      这个映射器返回了（这个函数） `hello`
+     *      这个 hello 被视图解析器拼接后就是：/WEB-INF/jsp/hello.jsp
+     *      然后 /WEB-INF/jsp/hello.jsp 中的资源就会被访问到
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping("/h1")  // 浏览器访问--> localhost:8080/h1
     public String hello(Model model) {
         // 封装数据
         // 向模型中添加属性 msg 与值，可以在JSP页面中取出并渲染
-        Model msg = model.addAttribute("msg", "Hello Silverfox here is SpringMVC use annotation printing!!!");
+        model.addAttribute("msg", "Hello Silverfox here is SpringMVC use annotation printing!!!");
 
-        return "hello";  // 会被视图解析器处理
+        return "hello";  // 会被视图解析器处理，解析 --> /WEB-INF/jsp/hello.jsp 中的内容
     }
 }
