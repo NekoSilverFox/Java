03c5c459f3ab35b810021c8fc9ed0ab2fc8ffe53
NekoSilverFox <weidufox@gmail.com>
2023-10-01 19:12:50 +0300

    增加 lab2

---
+++ 1000 - BigData/lab2/src/main/java/org/digdata/Main.java
@@ -0,0 +1,18 @@
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2023/10/1 19:08
+ * @Author : NekoSilverfox
+ * @FileName: Main
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ */
+
+package org.digdata;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
