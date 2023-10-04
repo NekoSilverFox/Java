081ab91617318e8fd2a19ae98f042984d2726dca
NekoSilverFox <weidufox@gmail.com>
2023-10-04 12:43:26 +0300

    增加 lab3 的两种实现方式

---
+++ 1000 - BigData/lab3_HttpClient5/server/src/test/java/com/bigdata/server/ServerApplicationTests.java
@@ -0,0 +1,24 @@
+/**
+ * -*- coding: utf-8 -*-
+ *
+ * @Time : 2023/10/3 17:29
+ * @Author : NekoSilverfox
+ * @FileName: ServerController
+ * @Software: IntelliJ IDEA
+ * @Versions: v0.1
+ * @Github ：https://github.com/NekoSilverFox
+ */
+
+package com.bigdata.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
